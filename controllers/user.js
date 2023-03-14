333042a9a29d70755476cca582ce4b00889576ff
joshuapent <joshdavidpent@gmail.com>
2023-03-13 23:33:06 -0500

    Added delete account functionality

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { Users } = require('../models')
 
 router.get('/', async (req, res, next) => {
     try {
--- models/Users.js
+++ models/Users.js
@@ -5,7 +5,6 @@ const usersSchema = new mongoose.Schema (
         name: {
             type: String,
             required: true,
-            unique: true
         },
         email: {
             type: String,
